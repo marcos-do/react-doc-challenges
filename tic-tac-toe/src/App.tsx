4f5d28b7a464da21abfe9d5b7b61bff88f9b6d01
marcos-do <42185858+marcos-do@users.noreply.github.com>
2024-08-23 17:05:12 -0300

    Adds React Tic Tac Toe

---
+++ tic-tac-toe/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <>
+      <h1>Tic Tac Toe Challenge</h1>
+      <div className="card">
+        <Game />
+      </div>
+    </>
+  );
+}
+
+export default App;
