a5a9722cbd817d81f0afed03386ae65e3064913c
Marcos <marcos.de.oliveira.madeira@gmail.com>
2024-08-25 17:20:23 -0300

    2. Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- tic-tac-toe/src/components/Board.tsx
+++ tic-tac-toe/src/components/Board.tsx
@@ -49,24 +49,28 @@ const Board: React.FC<BoardProps> = ({ xIsNext, squares, onPlay }) => {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const boardSize = 3;
+
+  const boardRows = Array(boardSize).fill(null);
+  const squaresPerRow = Array(boardSize).fill(null);
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardRows.map((_, i) => {
+        return (
+          <div key={i} className="board-row">
+            {squaresPerRow.map((_, k) => {
+              return (
+                <Square
+                  value={squares[i * boardSize + k]}
+                  onSquareClick={() => handleClick(i * boardSize + k)}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </>
   );
 };
